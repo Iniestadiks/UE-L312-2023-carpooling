b7ca4326bd2fde70155e861d21edbed34a1972ff
Madicke GUEYE <gueyemadicke0751@gmail.com>
2023-11-27 16:25:09 +0100

    Suppression des fichiers dupliqués de new-main

--- carpoolingads_read.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Controllers\CarpoolingAdsController;
-
-require __DIR__ . '/vendor/autoload.php';
-
-$controller = new CarpoolingAdsController();
-echo $controller->getCarpoolingAds();
-?>
