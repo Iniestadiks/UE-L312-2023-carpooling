39c3c93b41367d0a357b4c958c24dd9a8ac40d69
Madicke GUEYE <gueyemadicke0751@gmail.com>
2023-12-03 05:23:54 +0100

    Mise à jour de carpoolingads_create.php

--- carpooling-base/carpoolingads_create.php
+++ carpooling-base/carpoolingads_create.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Controllers\CarpoolingAdsController;
+use App\Services\CarsService;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $controller = new CarpoolingAdsController();
 echo $controller->createCarpoolingAd();
+
+$carsService = new CarsService();
+$cars = $carsService->getCars();
 ?>
 
 <p>Création d'une annonce de covoiturage</p>
-<form method="post" action="carpoolingads_create.php" name="carpoolingAdCreateForm">
+<form method="post" action="carpooling_create.php" name ="carpoolingCreateForm">
     <label for="title">Titre :</label>
     <input type="text" name="title">
     <br />
@@ -18,6 +22,13 @@ echo $controller->createCarpoolingAd();
     <br />
     <label for="price">Prix :</label>
     <input type="number" name="price" step="0.01">
+    <br />
+    <label for="cars">Voiture(s) :</label>
+    <?php foreach ($cars as $car): ?>
+        <?php $carName = $car->getBrand() . ' ' . $car->getModel() . ' ' . $car->getColor(); ?>
+        <input type="checkbox" name="cars[]" value="<?php echo $car->getId(); ?>"><?php echo $carName; ?>
+        <br />
+    <?php endforeach; ?>
     <br />
     <input type="submit" value="Créer une annonce">
 </form>
